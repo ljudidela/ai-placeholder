6ec125dc72e12c100eb73441f5229a3a852fe046
fedianin <fedianin@softlab.ru>
2025-11-27 20:23:26 +0300

    rollback model

--- api/ai-adapters/yandex-gpt.js
+++ api/ai-adapters/yandex-gpt.js
@@ -1,7 +1,7 @@
 export class YandexGPTAdapter {
   constructor() {
     this.name = "yandex-gpt";
-    this.modelUri = `gpt://${process.env.YANDEX_FOLDER_ID}/qwen3-235b-a22b-fp8/latest`;
+    this.modelUri = `gpt://${process.env.YANDEX_FOLDER_ID}/yandexgpt/rc`;
   }
 
   async generateCode(prompt) {
