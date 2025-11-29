bb64ffe71a8b250f10667c28921b7cfbd29ba0a4
fedianin <fedianin@softlab.ru>
2025-11-29 15:10:09 +0300

    change settings

--- api/ai-adapters/yandex-gpt.js
+++ api/ai-adapters/yandex-gpt.js
@@ -13,7 +13,7 @@ export class YandexGPTAdapter {
 
     const requestBody = {
       modelUri: this.modelUri,
-      completionOptions: { temperature: 0.1, maxTokens: 16000 },
+      completionOptions: { temperature: 0.3, maxTokens: 7000 },
       messages: [
         {
           role: "system",
